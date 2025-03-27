2a740ea3f1dd095f1ac2148ceb8c53b569433f74
Alper <alper.demir.fener@gmail.com>
2025-03-27 15:03:07 +0300

    Update conversation.router.js

--- backend/router/conversation.router.js
+++ backend/router/conversation.router.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createConversation, getConversations } from "../controllers/conversation.controller.js";
+import { createConversation, getConversations, getConversationById } from "../controllers/conversation.controller.js";
 
 const conversationRouter = express.Router();
 
 conversationRouter.post("/create", createConversation);
 conversationRouter.get("/get/:userId", getConversations);
-
+conversationRouter.get("/get/id/:conversationId", getConversationById);
 
 export default conversationRouter;
