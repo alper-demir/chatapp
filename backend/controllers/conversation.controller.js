74fce1533c5d8531cf4d3faa27f1782ffad4208f
Alper <alper.demir.fener@gmail.com>
2025-03-27 15:03:09 +0300

    Update conversation.controller.js

--- backend/controllers/conversation.controller.js
+++ backend/controllers/conversation.controller.js
@@ -2,13 +2,13 @@ import Conversation from "../models/conversation.model.js";
 
 export const createConversation = async (req, res) => {
 
-    const { participants } = req.body;
+    const { participants, isGroup, admins, groupName } = req.body;
     console.log("participants" + participants);
 
     const conversation = await Conversation.findOne({ participants: { $all: participants } });
 
     if (conversation?.length === 0 || !conversation) { // Create Conversation
-        const newConversation = await Conversation.create({ participants });
+        const newConversation = await Conversation.create({ participants, isGroup: isGroup ? isGroup : false, admins: admins ? admins : [], groupName: groupName ? groupName : "" });
         return res.status(201).json(newConversation);
     }
 
@@ -23,6 +23,18 @@ export const getConversations = async (req, res) => {
     try {
         const conversations = await Conversation.find({ participants: userId }).populate("lastMessage");
         return res.status(200).json(conversations);
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error!", error });
+    }
+}
+
+export const getConversationById = async (req, res) => {
+    // Get a conversation by conversationId
+    const { conversationId } = req.params;
+
+    try {
+        const conversation = await Conversation.findById(conversationId).populate("lastMessage");
+        return res.status(200).json(conversation);
     } catch (error) {
         return res.status(500).json({ message: "Internal server error!", error });
     }
