703563fea0f4ebc5e5909a3bb67b686856fda706
Alper <alper.demir.fener@gmail.com>
2025-03-29 16:57:32 +0300

    Update ChatLayout.jsx

--- frontend/src/layout/ChatLayout.jsx
+++ frontend/src/layout/ChatLayout.jsx
@@ -176,12 +176,6 @@ const ChatLayout = () => {
 
                             <MenuItems
                                 className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10"
-                                enter="transition ease-out duration-100"
-                                enterFrom="transform opacity-0 scale-95"
-                                enterTo="transform opacity-100 scale-100"
-                                leave="transition ease-in duration-75"
-                                leaveFrom="transform opacity-100 scale-100"
-                                leaveTo="transform opacity-0 scale-95"
                                 anchor="right start"
                             >
                                 <div className="py-1">
@@ -190,7 +184,7 @@ const ChatLayout = () => {
                                             <Link to="/chat/group-conversation"
                                                 className={`cursor-pointer ${active ? 'bg-green-50 text-green-700' : 'text-gray-700'
                                                     } group flex w-full items-center px-4 py-2 text-sm`}
-                                                // onClick={handleCreateGroup}
+                                            // onClick={handleCreateGroup}
                                             >
                                                 <HiOutlineUserGroup className="h-4 w-4 mr-3" />
                                                 Yeni Grup
