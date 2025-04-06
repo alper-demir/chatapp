6e7a56b85b85c4ae8c6dda932902ccf5fc327a86
Alper <alper.demir.fener@gmail.com>
2025-04-06 17:30:47 +0300

    Update conversation.model.js

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -5,7 +5,8 @@ const ConversationSchema = new mongoose.Schema({
     isGroup: { type: Boolean, default: false },
     groupName: { type: String },
     admins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Only for group conversations
-    lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' }
+    lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' },
+    description: { type: String }, // Grup açıklaması
 }, { timestamps: true });
 
 export default mongoose.model("Conversation", ConversationSchema);
