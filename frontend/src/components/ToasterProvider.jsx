2c912b70d370c7bcb0b2d8086c06b3f94abe4127
Alper <alper.demir.fener@gmail.com>
2025-04-17 13:25:58 +0300

    Bildirim için toaster eklendi

---
+++ frontend/src/components/ToasterProvider.jsx
@@ -0,0 +1,11 @@
+import { Toaster } from "react-hot-toast";
+
+const ToasterProvider = () => {
+    return (
+        <Toaster
+            position="top-right"
+        />
+    );
+};
+
+export default ToasterProvider;
