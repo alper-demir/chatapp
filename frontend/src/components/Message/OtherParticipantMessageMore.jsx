fb09a0587096ff466b04be04de297fde2043c7e4
Alper <alper.demir.fener@gmail.com>
2025-04-21 23:57:56 +0300

    Mesajlar için daha fazla alanı eklendi
    Mesaj bilgisi ve mesaj cevaplama özellikleri için dropdown menü ile temel atıldı.

---
+++ frontend/src/components/Message/OtherParticipantMessageMore.jsx
@@ -0,0 +1,57 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { BsInfoSquare } from "react-icons/bs";
+import { IoMdMore } from "react-icons/io";
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../store/modalSlice';
+import { useTranslation } from 'react-i18next';
+import { BsArrow90DegLeft } from "react-icons/bs";
+
+const OtherParticipantMessageMore = ({ conversationId, message, messageId, setReplyMessage }) => {
+
+    const dispatch = useDispatch();
+    const { t } = useTranslation();
+
+    const handleGroupInfoModal = () => {
+        dispatch(openModal({ modalType: "GroupInfoModal", modalData: { conversationId } }))
+    }
+
+    return (
+        <Menu as="div" className="relative inline-flex">
+            <MenuButton className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 cursor-pointer">
+                <IoMdMore className="text-lg" />
+            </MenuButton>
+
+            <MenuItems
+                className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-main-bg dark:bg-dark-main-bg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10"
+                anchor="right start"
+            >
+                <div className="py-1">
+                    <MenuItem>
+                        {({ active }) => (
+                            <div onClick={handleGroupInfoModal}
+                                className={`cursor-pointer ${active ? 'bg-sidebar-hover dark:bg-dark-dropdown-hover text-text dark:text-dark-text' : 'text-text dark:text-dark-text'
+                                    } group flex w-full items-center px-4 py-2 text-sm`}
+                            >
+                                <BsInfoSquare className="h-4 w-4 mr-3" />
+                                Mesaj bilgisi {messageId && <>{messageId}</>}
+                            </div>
+                        )}
+                    </MenuItem>
+                    <MenuItem>
+                        {({ active }) => (
+                            <div onClick={() => setReplyMessage(message)}
+                                className={`cursor-pointer ${active ? 'bg-sidebar-hover dark:bg-dark-dropdown-hover text-text dark:text-dark-text' : 'text-text dark:text-dark-text'
+                                    } group flex w-full items-center px-4 py-2 text-sm`}
+                            >
+                                <BsArrow90DegLeft className="h-4 w-4 mr-3" />
+                                Cevapla
+                            </div>
+                        )}
+                    </MenuItem>
+                </div>
+            </MenuItems>
+        </Menu>
+    )
+}
+
+export default OtherParticipantMessageMore
